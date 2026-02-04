6de0a6e4c0cbced49814859de1a834c49688d492
sushantebp <sushant.khadka@ebpearls.com>
2026-02-04 11:26:32 +0545

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:awesome/app/app.dart';
 import 'package:awesome/core/di/injection.dart';
+import 'package:awesome/core/services/background_handler.dart';
+import 'package:awesome/core/services/firebase_messaging_service.dart';
 import 'package:awesome/core/services/local_notification_service.dart';
 import 'package:awesome/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // FirebaseMessaging.onBackgroundMessage();
-
   configureDependencies();
-  getIt<LocalNotificationService>();
+  getIt<LocalNotificationService>().init();
+  getIt<FirebaseMessagingService>().init();
 
   runApp(const App());
 }
